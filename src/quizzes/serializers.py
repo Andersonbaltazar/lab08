e5ac891d690d060ade8eadf41f9990de85a64dbd
Andersonbaltazar <franklin.baltazar@tecsup.edu.pe>
2025-05-07 14:19:29 -0500

    serializrers completo

--- src/quizzes/serializers.py
+++ src/quizzes/serializers.py
@@ -23,3 +23,28 @@ class QuestionDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ['id', 'quiz', 'text', 'choices']
+
+class QuizSerializer(serializers.ModelSerializer):
+    """Serializer for the Quiz model"""
+    class Meta:
+        model = Quiz
+        fields = ['id', 'title', 'description', 'created_at']
+
+
+class QuizDetailSerializer(serializers.ModelSerializer):
+    """Serializer for Quiz model with nested questions and choices"""
+    questions = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Quiz
+        fields = ['id', 'title', 'description', 'created_at', 'questions']
+    
+    def get_questions(self, obj):
+        questions = obj.questions.all()
+        return QuestionDetailSerializer(questions, many=True).data
+
+
+class AnswerSerializer(serializers.Serializer):
+    """Serializer for answer validation"""
+    question_id = serializers.IntegerField()
+    choice_id = serializers.IntegerField()
