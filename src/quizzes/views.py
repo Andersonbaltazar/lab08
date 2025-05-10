581d1974e36b743d716e7bfa12327ceb66dd7e06
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-05-10 15:57:52 -0500

    Import Serializers

--- src/quizzes/views.py
+++ src/quizzes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, status
+from rest_framework import action
+from rest_framework import Response
+
+from .models import Quiz, Question, Choice
+
+from .serializers import (
+    QuizSerializer, QuizDetailSerializer,
+    QuestionSerializer, QuestionDetailSerializer,
+    ChoiceSerializer, AnswerSerializer
+)
