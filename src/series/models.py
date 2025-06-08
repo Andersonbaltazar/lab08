e386700f14f6520e8ca7d94fde3dc3d5bfc9632d
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-06-07 23:23:20 -0500

    New Atribute for Model Serie

--- src/series/models.py
+++ src/series/models.py
@@ -11,4 +11,5 @@ class Serie(models.Model):
 	name = models.CharField(max_length=100)
 	release_date = models.DateField()
 	rating = models.IntegerField(default=0)
+	image = models.CharField(max_length=200, blank=True, null=True)
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
