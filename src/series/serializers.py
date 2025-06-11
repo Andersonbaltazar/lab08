d4ca2e07ed06b12075d72bc1eabc6c88b10174bf
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-06-11 15:14:56 -0500

    Final

--- src/series/serializers.py
+++ src/series/serializers.py
@@ -1,19 +1,14 @@
 from rest_framework import serializers
 from .models import Serie, Category
 
-class SerieSerializer(serializers.ModelSerializer):
-    category_description = serializers.SerializerMethodField()
 
+class SerieSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Serie
-        fields = ['id', 'name', 'release_date', 'rating', 'image', 'category_description']
-
-    def get_category_description(self, obj):
-        return obj.category.description if obj.category else None
+        fields = ('id', 'name', 'release_date', 'rating', 'image', 'category')
 
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'description')
-    
