b51fbcf46ccac4519b349704dc69c8b80a85b1de
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-06-18 12:56:43 -0500

    LoginSerializer, View and URL

--- src/series/serializers.py
+++ src/series/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Serie, Category
+from django.contrib.auth.models import User
 
 class SerieSerializer(serializers.ModelSerializer):
     category_description = serializers.SerializerMethodField()
@@ -16,3 +17,12 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(write_only=True)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
